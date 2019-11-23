4c318dac25e5c357491c87d80a9bfa56b63a031c
vichuvichu <54546662+vichuvichu@users.noreply.github.com>
2019-11-23 18:14:15 +0530

    Update application account.py

--- application account.py
+++ application account.py
@@ -3,17 +3,23 @@ data={}
 def create():
   data['acc_no']=random.randint(1000000000,9999999999)
   data['bal']=10000
-  print('Your account number is:',data['acc_no'])
+  print('\nYour account number is:',data['acc_no'])
   #getting users name----------------------------------
   namerun=True
   while namerun:
     namerun=False
     data['name']=input('Type your name: ')
+    if data['name'].strip()=='':
+      print('Yedhachu type panala...\nOnumae illa anga!')
+      namerun=True
+      continue
     for i in data['name'].lower():
       if not 97<=ord(i)<=122:
-        print('As far as I know name contains only alphabets, not numbers or anyother!\nnow try again')
+        print('\nAs far as I know name contains only alphabets, not numbers or anyother!\nnow try again\n')
         namerun=True
         break
+      
+      
   #user name obtained-------------------------------
   phrun=True
   while phrun:
@@ -21,20 +27,20 @@ def create():
     try:
       data['ph']=int(input('Type your phone number: '))
     except ValueError:
-      print('A phone number contains number not anything else\nTry again')
+      print('\nA phone number contains number not anything else\nTry again\n')
       phrun=True
       continue
     if len(str(data['ph']))!=10:
-      print('My god! phone numbers have 10 numbers like 7550309683 not {} this is not even a phone number!\nTry again'.format(data['ph']))
+      print('\nMy god! phone numbers have 10 numbers like 7550309683 not {} this is not even a phone number!\nTry again\n'.format(data['ph']))
       phrun=True
       continue
   #phone number vaagiyatchu----------------------------
   passrun=True
   while passrun:
     passrun=False
-    data['ps']=input('\nA Password must contain atleast 8 characters, so keep that in mind and type one: ')
+    data['ps']=input('\nA Password must contain atleast 8 characters, so keep that in mind and \ntype one: ')
     if len(data['ps'])<8:
-      print('What did I just told you??\nTry again')
+      print('\nWhat did I just told you??\nTry again\n')
       passrun=True
       continue
   #password done------------------------------------
@@ -47,7 +53,7 @@ def login():
     name=input('Type your name: ')
     if name!=data['name']:
       namerun=True
-      print('No such username')
+      print('\nNo such username\n')
       continue
   psrun=True
   while psrun:
@@ -62,31 +68,39 @@ def login():
     acrun=False
     acno=input('Type your account number: ')
     if int(acno)!=data['acc_no']:
-      print('Wrong!\nTry again')
+      print('\nWrong!\nTry again\n')
       acrun=True
       continue
   print('Success')
 #getting user choice--------------------------------------------
 
 chrun=True
+i=0
+first=True
 while chrun:
   chrun=False
   try:
     ch=int(input("1.Create new account\n2.Login to existing account\nEnter your choice: "))
 
   except ValueError:
-    print('Type an integer not characters or symbols')
+    print('\nType an integer not characters or symbols\nTry again\n')
     chrun=True
     continue
 
   if ch==1:
     create()
+    first=False
     chrun=True
     continue
   elif ch==2:
+    if first:
+      i+=1
+      print('\ninu yedhu create yae panala, Yaara yematha paakura?\nCreate new kudu{}\n'.format(i) if i==1 else '\nAdey yevalovaati soldradhu!\ninnu yedhu create panala {} time soldraen\n'.format(i))
+      chrun=True
+      continue
     login()
   else:
-    print('Invalid choice choices are between(1-2)')
+    print('\nInvalid choice choices are between(1-2)\n')
     chrun=True
     continue
 #user choice done-----------------------------------------------
